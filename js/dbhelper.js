c093fcad565ac0b38f34b6b777bf10ae0de0a141
Jérémie Litzler <contact@jeremielitzler.net>
2018-02-18 15:04:59 +0100

    fix:setup on local env

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,8 +8,9 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    const port = 80; // Change this to your server port
+    const appAlias = "/mws-restaurant-stage-1";
+    return `http://localhost:${port}${appAlias}/data/restaurants.json`;
   }
 
   /**
