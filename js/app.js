c2209c218bbc74b2357f168347e1db40acda9bbb
Jérémie Litzler <contact@jeremielitzler.net>
2018-02-21 20:16:51 +0100

    #1-feat:redesign the header, map and buttons

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 const origin = location.origin;
+console.log("origin is ", origin);
 let isSsl = location.protocol.indexOf("https") !== -1;
 let appAlias = "/mws-restaurant-stage-1";
 
-if (location.hostname === "mws-restaurant-stage-1-webdevjlprojects.c9users.io") {
-    appAlias = "";
+if (
+  location.hostname === "mws-restaurant-stage-1-webdevjlprojects.c9users.io"
+) {
+  appAlias = "";
 }
