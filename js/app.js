fa74691a7b826ca7d942da6fec0b6fd92d126eb9
Jérémie Litzler <contact@jeremielitzler.net>
2018-02-28 20:11:23 +0100

    chore: update google api key

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+const GOOGLE_MAPS_API_KEY = "AIzaSyBXysE433qRY0W9gup0-_N5UF_0ObJK3oc";
 const origin = location.origin;
 console.log("origin is ", origin);
 let isSsl = location.protocol.indexOf("https") !== -1;
