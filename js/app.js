93e89c21bb46dd1f228243b2b39c6574d9590787
Jérémie LITZLER <2410556+JeremieLitzler@users.noreply.github.com>
2018-03-14 07:57:12 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const origin = location.origin;
 console.log("origin is ", origin);
 let isSsl = location.protocol.indexOf("https") !== -1;
 let appAlias = "/mws-restaurant-stage-1";
-if (location.hostname === "mws-restaurant-stage-1-webdevjlprojects.c9users.io") {
+if (location.hostname === "mws-restaurant-stage-1-webdevjlprojects.c9users.io" ||
+    location.hostname === "agitated-shockley-450c4d.netlify.com") {
     appAlias = "";
 }
